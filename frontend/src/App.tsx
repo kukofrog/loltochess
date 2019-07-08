e19eb07b481a9d7e935503935ee3805406725b0e
Dotsung <andy395023@gmail.com>
2019-07-08 09:17:57 +0900

    라우팅, install 등등 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserHistory } from 'history';
+
+import { Router, Route, Switch } from "react-router-dom";
+import { Home } from 'pages';
+
+const history = createBrowserHistory();
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = () => (
+    <>
+    Home
+    </>
+)
+
+
+export default Home;
